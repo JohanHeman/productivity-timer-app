cbf49436cf8db72fd4a30f747e9e875e1910cafd
Johan Heman <jojo2001@hotmail.se>
2026-03-10 11:01:26 +0100

    Started inplementing face design pattern

---
+++ ProductivityTimer.UseCase/Facade/IWorkFacade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductivityTimer.Application.Facade
+{
+    internal interface IWorkFacade
+    {
+        event Action<TimeSpan>? TimeChanged;
+        Task StartAsync();
+        Task PauseAsync();
+        Task StopAndSaveAsync();
+
+        TimeSpan GetRemainingTime();
+    }
+}
---
+++ ProductivityTimer.UseCase/Facade/WorkFacade.cs
@@ -0,0 +1,53 @@
+﻿using ProductivityTimer.Application.Facade;
+using ProductivityTimer.Application.Interfaces;
+using ProductivityTimer.Domain.Interfaces;
+using ProductivityTimer.Domain.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductivityTimer.Application
+{
+    internal class WorkFacade : IWorkFacade
+    {
+        private DateTime? _StartedSessionAt;
+        private readonly ITimerService _timerService;
+
+        private readonly IWorkSessionRepository _workSessionRepository;
+        public WorkFacade(ITimerService timerService, IWorkSessionRepository workSessionRepository)
+        {
+            _timerService = timerService;
+            _workSessionRepository = workSessionRepository;
+        }
+        public event Action<TimeSpan>? TimeChanged;
+
+        public TimeSpan GetRemainingTime() => _timerService.GetRemainingTime();
+
+        public async Task PauseAsync() => await _timerService.PauseTimerAsync();
+
+        public async Task StartAsync()
+        {
+            _StartedSessionAt = DateTime.Now;
+            await _timerService.StartTimerAsync();
+        }
+
+        public async Task StopAndSaveAsync()
+        {
+            await _timerService.StopTimerAsync();
+
+            if (_StartedSessionAt == null)
+                return;
+
+            var endedAt = DateTime.Now;
+            var session = new WorkSession
+            {
+                StartedAt = _StartedSessionAt.Value,
+                EndedAt = endedAt,
+                Duration = endedAt - _StartedSessionAt.Value
+            };
+
+            await _workSessionRepository.SaveSessionAsync(session);
+            _StartedSessionAt = null;
+        }
+    }
+}
