f7b7219547faa48edf30b667706fd6b49faaaf12
Johan Heman <jojo2001@hotmail.se>
2026-03-13 08:58:12 +0100

    Added some more comments

--- ProductivityTimer/ViewModels/HistoryPageViewModel.cs
+++ ProductivityTimer/ViewModels/HistoryPageViewModel.cs
@@ -25,37 +25,45 @@ namespace ProductivityTimer.ViewModels
         public ICommand ReviewDailyCommand { get; }
         public ICommand ReviewWeeklyCommand { get; }
         public ICommand ReviewMonthlyCommand { get; }
+        public ICommand NavigateHomeCommand { get; }
+
+        private TimeSpan TotalHours { get; set; }
+        public string TotalHoursText => $"{TotalHours.Hours}h {TotalHours.Minutes}m"; // formating  for display purposes
 
         protected virtual void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
+
         private async Task ReviewDailyAsync()
         {
             TotalHours = await _statisticService.GetTotalHoursAsync(DateTime.Today, TimeRange.Day);
             OnPropertyChanged(nameof(TotalHours));
             OnPropertyChanged(nameof(TotalHoursText));
         }
+
+
         private async Task ReviewWeeklyAsync()
         {
             TotalHours = await _statisticService.GetTotalHoursAsync(DateTime.Today, TimeRange.Week);
             OnPropertyChanged(nameof(TotalHours));
             OnPropertyChanged(nameof(TotalHoursText));
         }
+
+
         private async Task ReviewMonthlyAsync()
         {
             TotalHours = await _statisticService.GetTotalHoursAsync(DateTime.Today, TimeRange.Month);
             OnPropertyChanged(nameof(TotalHours));
             OnPropertyChanged(nameof(TotalHoursText));
         }
+
+
         private async Task GoToHomeAsync()
         {
             await Shell.Current.GoToAsync("//MainPage");
         }
-        public ICommand NavigateHomeCommand { get; }
-        private TimeSpan TotalHours { get; set; }
-        public string TotalHoursText => $"{TotalHours.Hours}h {TotalHours.Minutes}m"; // formating 
-
 
 
     }
