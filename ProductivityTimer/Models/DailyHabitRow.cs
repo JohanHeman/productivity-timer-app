9f78798040e4f226d7f0ef7fb8c52cd8249b92f5
Johan Heman <jojo2001@hotmail.se>
2026-03-13 15:51:04 +0100

    Set up DailyHabitRow model to hav more editable properties

--- ProductivityTimer/Models/DailyHabitRow.cs
+++ ProductivityTimer/Models/DailyHabitRow.cs
@@ -9,14 +9,59 @@ namespace ProductivityTimer.Models
     public class DailyHabitRow : INotifyPropertyChanged
     {
         public DailyHabit Habit { get; set; }
-        public int Streak { get; set; }
-        private bool _isCompleted { get; set; }
+
+        private int _streak;
+        public int Streak
+        {
+            get => _streak;
+            set
+            {
+                if (_streak == value) return; // if streak = value, no need to udate anything 
+                _streak = value;
+                OnPropertyChanged(nameof(Streak));
+            }
+        }
+
+        private bool _isCompleted;
         public string Name => Habit.Name;
+
+        private bool _isEditing;
+        public bool IsEditing
+        {
+            get => _isEditing;
+            set
+            {
+                if (_isEditing == value) return;
+                _isEditing = value;
+                OnPropertyChanged(nameof(IsEditing));
+                OnPropertyChanged(nameof(IsEdting));
+            }
+        }
+
+        public bool IsEdting
+        {
+            get => IsEditing;
+            set => IsEditing = value;
+        }
+
+        private string _editableName = string.Empty;
+        public string EditableName
+        {
+            get => _editableName;
+            set
+            {
+                if (_editableName == value) return; // if editableName = value, no need to update anything
+                _editableName = value;
+                OnPropertyChanged(nameof(EditableName));
+            }
+        }
+
         public bool IsCompleted
         {
             get => _isCompleted;
             set
             {
+                if (_isCompleted == value) return; // if isCompleted = value, no need to update anything
                 _isCompleted = value;
                 OnPropertyChanged(nameof(IsCompleted));
             }
